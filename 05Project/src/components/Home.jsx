8dea4713dc4fe26ca58dd5ec902ca04cac2ef1cd
ayushigarg24 <ayushigarg24@Ayushi>
2025-07-29 13:36:00 +0530

    changes

--- 05Project/src/components/Home.jsx
+++ 05Project/src/components/Home.jsx
@@ -32,15 +32,13 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-[60%] h-full object-cover" src="https://plopdo.com/wp-content/uploads/2021/07/Screenshot-1-1210x642.png?crop=1" alt="image1" />
                 </div>
             </aside>
 
-            <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-            </div>
+          
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Frontend Developer</h1>
         </div>
     );
 }
